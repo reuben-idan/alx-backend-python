13ea7755f0520c437c6aeb1835ef17f7f5436abb
reuben-idan <nobeadonis2@gmail.com>
2025-05-17 11:55:35 +0000

    Create generator that streams rows from an SQL database

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+def stream_users():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="your_username",
+        password="your_password",
+        database="ALX_prodev"
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    connection.close()
