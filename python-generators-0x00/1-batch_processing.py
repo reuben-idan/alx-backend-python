e3cd2b57e0aaf8891a0b3dce6850aec1e472213f
reuben-idan <nobeadonis2@gmail.com>
2025-05-17 12:00:05 +0000

    Create batch processing Large Data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="your_username",
+        password="your_password",
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    batch = []
+    for row in cursor:
+        batch.append(row)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+
+    if batch:  # yield any remaining users
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
