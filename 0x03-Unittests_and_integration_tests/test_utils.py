4fcb375d5ffc176fe5751dc83fe611efb841d274
reuben-idan <nobeadonis2@gmail.com>
2025-05-31 12:38:40 +0000

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map"""
+"""
+Unit tests for utils.access_nested_map
+"""
 
 import unittest
 from parameterized import parameterized
@@ -7,7 +9,7 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests for the access_nested_map function"""
+    """Unit tests for the access_nested_map function"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -15,5 +17,15 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Test access_nested_map returns correct value"""
+        """Test normal behavior of access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_message):
+        """Test KeyError is raised with correct message"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_message}'")
