381cd06d4e71533f17077f8f3aa932295a92bf82
reuben-idan <nobeadonis2@gmail.com>
2025-06-01 17:50:09 +0000

    update code snippet

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),           # Your main API endpoints
+    path('api-auth/', include('rest_framework.urls')),  
 ]
