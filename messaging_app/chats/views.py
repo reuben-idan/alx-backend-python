7aa7c722848a8d4afb563ff9bd8e1952c6daad61
reuben-idan <nobeadonis2@gmail.com>
2025-06-01 17:04:37 +0000

    Build api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return conversations involving the authenticated user
+        return self.queryset.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+        conversation.save()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter messages by conversations the user is a part of
+        return self.queryset.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
