fde162c25b0c07abad1101223289ff2524a65b45
reuben-idan <nobeadonis2@gmail.com>
2025-06-14 13:01:31 +0000

    Update code snippet

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,17 +1,41 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
-from .models import Message, MessageHistory
+from django.contrib.auth.models import User
+from .models import Message, MessageHistory, Notification
+
 
 @receiver(pre_save, sender=Message)
 def log_message_edits(sender, instance, **kwargs):
-    if instance.pk:  # message already exists (not a new one)
+    """
+    Logs the old content of a message before it's edited,
+    and creates a history entry in the MessageHistory model.
+    """
+    if instance.pk:
         try:
             old_message = Message.objects.get(pk=instance.pk)
             if old_message.content != instance.content:
                 MessageHistory.objects.create(
                     message=old_message,
-                    old_content=old_message.content
+                    old_content=old_message.content,
+                    edited_by=instance.edited_by  # Capture who made the edit
                 )
-                instance.edited = True  # mark it as edited
+                instance.edited = True
         except Message.DoesNotExist:
-            pass  # New message, do nothing
+            pass  # Likely a new message, no previous content to track
+
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    """
+    Automatically delete all related messages, notifications,
+    and message histories when a user account is deleted.
+    """
+    # Delete messages where the user is sender or receiver
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete notifications for the user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete message histories where user edited
+    MessageHistory.objects.filter(edited_by=instance).delete()
