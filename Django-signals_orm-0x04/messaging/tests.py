23727a5c1f1969604a1c30ade3e22203b46b00c3
reuben-idan <nobeadonis2@gmail.com>
2025-06-14 12:07:06 +0000

    Implement Signals for User Notifications

---
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+class MessagingSignalTest(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username='sender', password='pass')
+        self.receiver = User.objects.create_user(username='receiver', password='pass')
+
+    def test_notification_created_on_message(self):
+        message = Message.objects.create(sender=self.sender, receiver=self.receiver, content="Hello!")
+        self.assertEqual(Notification.objects.count(), 1)
+        notification = Notification.objects.first()
+        self.assertEqual(notification.user, self.receiver)
+        self.assertEqual(notification.message, message)
